5095ca5a48ba00ac32ab1c52dc08464320aa23fe
carlosrs14 <crincones578@gmail.com>
2025-05-20 23:20:47 -0500

    config para enviar la url de las imagenes al front

--- app/Models/FotoPropiedad.php
+++ app/Models/FotoPropiedad.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class FotoPropiedad extends Model
 {
     protected $table = 'fotospropiedad';
-    
+    protected $appends = ['full_url'];
+
     protected $fillable = [
         'url',
         'propiedad_id'
@@ -15,5 +17,9 @@ class FotoPropiedad extends Model
 
     public function propiedad() {
         return $this->belongsTo(Propiedad::class);
+    }
+
+    public function getFullUrlAttribute() {
+        return asset(Storage::url($this->url));    
     }
 }
